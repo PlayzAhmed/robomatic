923875eb2dc3f46f623a4f20c7dbfbe116f28a5a
PlayzAhmed <78406761+PlayzAhmed@users.noreply.github.com>
2024-06-27 23:42:03 +0300

    Add files via upload

---
+++ Databases/powerAndRoot.js
@@ -0,0 +1,150 @@
+let power = [
+    {
+        q: "What is 2³?",
+        a: "8"
+    },
+    {
+        q: "Calculate 4².",
+        a: "16"
+    },
+    {
+        q: "What is the value of 5⁴? ",
+        a: "625"
+
+    },
+    {
+        q: "If x⁰ equals 1 for any nonzero number x, what is the value of 3⁰? ",
+        a: "1"
+        
+    },
+    {
+        q: "What is the result of 6⁵?",
+        a: "7776"
+        
+    },
+    {
+        q: "If y¹ equals y for any number y, what is the value of 7¹? ",
+        a: "7"
+        
+    },
+    {
+        q: "Evaluate 10². ",
+        a: "100"
+        
+    },
+    {
+        q: "If a² = 64, what is the value of a?",
+        a: "8"
+        
+    },
+    {
+        q: "What is the value of 9³? ",
+        a: "729"
+        
+    },
+    {
+        q: " What is the result of 2⁴?",
+        a: "16"
+        
+    },
+    {
+        q: "If a³= 27, what is the value of a?",
+        a: "3"
+        
+    },
+    {
+        q: "What is the value of (3²)³?  ",
+        a: "729"
+        
+    },
+    {
+        q: "If 4ˣ= 256, what is the value of x? ",
+        a: "4"
+        
+    },
+    {
+        q:" What is the result of 5³? ",
+        a: "125"
+        
+    },
+    {
+        q: "What is  2³?",
+        a: "8"
+        
+    },
+]
+ let roots = [ 
+    {
+        q: "What is the square root of 9 (√9)?",
+        a: "3"
+    },
+    {
+        q: "Find the square root of 4 (√4).",
+        a: "2"
+    },
+    {
+        q: "Calculate the square root of 16 (√16).",
+        a: "4"
+
+    },
+    {
+        q: "What is the square root of 1 (√1)? ",
+        a: "1"
+        
+    },
+    {
+        q: "Determine the square root of 25 (√25).",
+        a: "5"
+        
+    },
+    {
+        q: "Find the cubic root of 8 (∛8).",
+        a: "2"
+        
+    },
+    {
+        q: "Calculate the cubic root of 27 (∛27).",
+        a: "3"
+        
+    },
+    {
+        q: "What is the cubic root of 1 (∛1)?",
+        a: "1"
+        
+    },
+    {
+        q: "Determine the cubic root of 64 (∛64).",
+        a: "4"
+        
+    },
+    {
+        q: " Find the cubic root of 125 (∛125).",
+        a: "5"
+        
+    },
+    {
+        q: "Determine the cubic root of 512 (∛512).",
+        a: "8"
+        
+    },
+    {
+        q: "What is the square root of 121 (√121)?",
+        a: "11"
+        
+    },
+    {
+        q: "Find the square root of 144 (√144).",
+        a: "12"
+        
+    },
+    {
+        q:"Calculate the cubic root of 216 (∛216). ",
+        a: "6"
+        
+    },
+    {
+        q: "Determine the square root of 36 (√36).",
+        a: "6"
+        
+    },
+ ]
